0b372cbbc543b31eed7f4d4b745681b26bbbe807
AlanCoding <arominge@redhat.com>
2020-07-13 15:41:24 -0400

    Allow running all examples

--- test/data/run.sh
+++ test/data/run.sh
@@ -1,17 +1,28 @@
 #!/usr/bin/env bash
 
+run_example () {
+  echo ""
+  echo "Running Example: $1"
+  IMAGE_NAME=exec-env-$1
+  docker image rm $IMAGE_NAME -f
+  rm -rf bc
+  ansible-builder build -f $1/execution-environment.yml --container-runtime=docker -c bc --tag $IMAGE_NAME
+  ansible-runner --playbook $1.yml run $1 -v
+  return 0
+}
+
+cd examples
+
 if [ ! $1 ]
 then
-  echo "Provide an argument which is the name of a subfolder"
-  return 1 2>/dev/null
-  exit 1
+  for dir in */;
+  do
+      if [ $dir != "bc/" ]; then
+        run_example ${dir%*/}
+      fi
+  done
+else
+  run_example $1
 fi
 
-echo "Running example $1"
-
-IMAGE_NAME=exec-env-$1
-
-docker image rm $IMAGE_NAME -f
-rm -rf examples/bc
-ansible-builder build -f examples/$1/execution-environment.yml --container-runtime=docker -c examples/bc --tag $IMAGE_NAME
-ansible-runner --playbook $1.yml run examples/$1 -v
+cd ..
