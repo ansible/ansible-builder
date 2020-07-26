5f5961fb6b108570bbfc71b4c00de5f1aa212b7f
AlanCoding <arominge@redhat.com>
2020-07-26 18:04:37 -0400

    Add test for invalid option

--- test/test_cli.py
+++ test/test_cli.py
@@ -12,8 +12,8 @@ def cli_mock():
     @contextmanager
     def rf(args):
         with mock.patch.object(sys, 'argv', args):
-            with pytest.raises(SystemExit):
-                yield
+            with pytest.raises(SystemExit) as exc:
+                yield exc
     return rf
 
 
@@ -22,8 +22,18 @@ def test_custom_image(exec_env_definition_file, tmpdir, cli_mock):
     path = str(exec_env_definition_file(content=content))
 
     args = ['ansible-builder', 'build', '-f', path, '-b', 'my-custom-image', '-c', str(tmpdir)]
-    with cli_mock(args):
+    with cli_mock(args) as sys_exit:
         run()
+
+    assert sys_exit.value.code == 0
 
     with open(os.path.join(str(tmpdir), 'Containerfile')) as f:
         assert 'FROM my-custom-image' in f.read()
+
+
+def test_invalid_path(tmpdir, cli_mock):
+    args = ['ansible-builder', 'build', '-f', 'foo/invalid', '-c', str(tmpdir)]
+    with cli_mock(args) as sys_exit:
+        run()
+
+    assert sys_exit.value.code != 0
