6e19cbb3662328ca51a2b5734872db02c0a7c363
AlanCoding <arominge@redhat.com>
2020-07-26 22:28:00 -0400

    Echo command for user to use

--- test/data/run.sh
+++ test/data/run.sh
@@ -6,7 +6,13 @@ run_example () {
   IMAGE_NAME=exec-env-$1
   docker image rm $IMAGE_NAME -f
   rm -rf bc
+  echo ""
+  echo "Running:"
+  echo "ansible-builder build -f examples/$1/execution-environment.yml --container-runtime=docker -c examples/bc --tag $IMAGE_NAME"
   ansible-builder build -f $1/execution-environment.yml --container-runtime=docker -c bc --tag $IMAGE_NAME
+  echo ""
+  echo "Running:"
+  echo "ansible-runner --playbook $1.yml run examples/$1 -v"
   ansible-runner --playbook $1.yml run $1 -v
   return 0
 }
