112b2b108347fc66190ec14057ace1d161df1c07
Alan Rominger <arominge@redhat.com>
2020-08-11 15:00:28 -0400

    Use the force option when removing images

--- test/integration/conftest.py
+++ test/integration/conftest.py
@@ -25,7 +25,7 @@ def ee_tag(request, cli, container_runtime):
     image_name = 'builder-test-' + str(uuid.uuid4())[:10]
 
     def delete_image():
-        cli(f'{container_runtime} rmi {image_name}')
+        cli(f'{container_runtime} rmi -f {image_name}')
 
     request.addfinalizer(delete_image)
     return image_name
