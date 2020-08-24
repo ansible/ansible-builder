5deb91a69575889548fb24c08c8023f35c57e3b7
Alan Rominger <arominge@redhat.com>
2020-08-24 16:25:57 -0400

    Do not use dockerhub, use quay

--- ansible_builder/constants.py
+++ ansible_builder/constants.py
@@ -1,5 +1,5 @@
 default_file = 'execution-environment.yml'
-default_base_image = 'docker.io/ansible/ansible-runner:devel'
+default_base_image = 'quay.io/ansible/ansible-runner:devel'
 default_tag = 'ansible-execution-env:latest'
 default_build_context = 'context'
 runtime_files={
