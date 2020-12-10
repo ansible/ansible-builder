7305ea9aa01d418c252644a0fc279d22c130dccf
Alan Rominger <arominge@redhat.com>
2020-12-10 10:45:18 -0500

    Remove files and point to new AWX EE home

--- test/data/awx/run.sh
+++
@@ -1,4 +0,0 @@
-docker image rm exec-env-ansible-venv -f
-rm -rf examples/bc
-ansible-builder build -f examples/awx/execution-environment.yml --container-runtime=docker -c examples/bc --tag exec-env-ansible-venv
-ansible-runner --playbook test.yml run examples/awx -vvv
