db9e74a0b66aa4b535bf6946c5288f1c2619c133
David Shrewsbury <Shrews@users.noreply.github.com>
2022-03-21 11:00:54 -0400

    Normalize python package names (#359)
    Normalize python package names during sanitization
    
    Fixes #351
    Older versions of pip cannot handle duplicate entries of a python package within a requirements file. E.g.,
    python-dateutil>=2.8.2
    python_dateutil
    
    Newer versions of pip can handle this fine.
    ansible-builder sanitizes requirements from multiple sources (collections) by removing duplicates. However, it doesn't currently account for package names that are not normalized (python-dateutil should be the same as python_dateutil). So in an EE environment with older versions of pip, the pip install portion could fail with an error similar to:
    ERROR: Double requirement given: python-dateutil>=2.8.2 (from -r /tmp/r.txt (line 2)) (already in python_dateutil (from -r /tmp/r.txt (line 1)), name='python-dateutil')
    
    This change normalizes the package names (since the underlying parsing library is not doing that for us).
    
    Reviewed-by: Matt Martz <matt@sivel.net>
    Reviewed-by: Shane McDonald <me@shanemcd.com>

--- test/integration/test_introspect_cli.py
+++ test/integration/test_introspect_cli.py
@@ -35,6 +35,7 @@ def test_introspect_write_python(cli, data_dir, tmp_path):
     assert dest_file.read_text() == '\n'.join([
         'pyvcloud>=14  # from collection test.metadata',
         'pytz  # from collection test.reqfile',
+        'python-dateutil>=2.8.2  # from collection test.reqfile',
         'tacacs_plus  # from collection test.reqfile',
         'pyvcloud>=18.0.10  # from collection test.reqfile',
         '',
@@ -48,7 +49,8 @@ def test_introspect_write_python_and_sanitize(cli, data_dir, tmp_path):
     assert dest_file.read_text() == '\n'.join([
         'pyvcloud>=14,>=18.0.10  # from collection test.metadata,test.reqfile',
         'pytz  # from collection test.reqfile',
-        'tacacs_plus  # from collection test.reqfile',
+        'python-dateutil>=2.8.2  # from collection test.reqfile',
+        'tacacs-plus  # from collection test.reqfile',
         '',
     ])
 
