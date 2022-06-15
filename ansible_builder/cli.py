bde6b5367babf7bf03d26566b8384bf101435747
David Shrewsbury <Shrews@users.noreply.github.com>
2022-06-15 13:05:30 -0400

    Fix default tag not being used (#395) (#396)
    (cherry picked from commit 070a0f68a4a549acd11642b116e34ecf89b80d55)

--- ansible_builder/cli.py
+++ ansible_builder/cli.py
@@ -235,7 +235,7 @@ def parse_args(args=sys.argv[1:]):
     args = parser.parse_args(args)
 
     # Tag default must be handled differently. See comment for --tag option.
-    if 'tag' not in vars(args):
+    if 'tag' not in vars(args) or not args.tag:
         args.tag = [constants.default_tag]
 
     return args
